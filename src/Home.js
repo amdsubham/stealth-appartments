ea19bfde77a8d863085613ed3ace0970ccd6d328
Subham Routray <subhamroutray@Subhams-MacBook-Pro.local>
2022-01-09 20:35:59 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,36 @@
 import React from "react";
 import app from "./base";
+import { Card, Col, Row } from 'antd';
+import './App.css';
+import {appData} from './mockdata'
+
+const renderAppartments = ()=> {
+  return appData.map(item=>(
+    <Col span={8}>
+    <Card style={{ width: 300 }} title={item.apartment_name} bordered={false}
+        cover={<img alt="example"  style={{ width: 300, height:300 }} src={item.image_url} />}
+    >
+      <p>{item.address}</p>
+      <p>Status: Yet to Update</p>
+      <p>Tier: {item.tier}</p>
+    </Card>
+  </Col>
+  ))
+}
+
 
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+    <div style={{display:'flex', justifyContent:'space-between	'}}> 
+    <h1>Appartments Details</h1>
       <button onClick={() => app.auth().signOut()}>Sign out</button>
+    </div>
+      <div className="site-card-wrapper">
+    <Row gutter={16}>
+{renderAppartments()}
+    </Row>
+  </div>
     </>
   );
 };
--- src/Login.js
+++ src/Login.js
@@ -2,16 +2,19 @@ import React, { useCallback, useContext } from "react";
 import { withRouter, Redirect } from "react-router";
 import app from "./base.js";
 import { AuthContext } from "./Auth.js";
+import 'antd/dist/antd.css';
+import './index.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+
 
 const Login = ({ history }) => {
   const handleLogin = useCallback(
     async event => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
+      const { email, password } = event;
       try {
         await app
           .auth()
-          .signInWithEmailAndPassword(email.value, password.value);
+          .signInWithEmailAndPassword(email, password);
         history.push("/");
       } catch (error) {
         alert(error);
@@ -26,10 +29,81 @@ const Login = ({ history }) => {
     return <Redirect to="/" />;
   }
 
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const renderForm =()=>(
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={handleLogin}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
   return (
     <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
+      <h1>Stealth Apartment Project</h1>
+      <h3>If you are not Kaushik || Subham. Please Don't Login</h3>
+      {renderForm()}
+      {/* <form onSubmit={handleLogin}>
         <label>
           Email
           <input name="email" type="email" placeholder="Email" />
@@ -39,7 +113,7 @@ const Login = ({ history }) => {
           <input name="password" type="password" placeholder="Password" />
         </label>
         <button type="submit">Log in</button>
-      </form>
+      </form> */}
     </div>
   );
 };
